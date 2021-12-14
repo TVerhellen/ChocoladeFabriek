7eebacfd5974f5132f3d07d8d130cfb9022523fe
Tijs Verhellen <tijs.verhellen@gmail.com>
2021-12-14 13:40:47 +0100

    fixed form1

--- Chocolade/Program.cs
+++ Chocolade/Program.cs
@@ -17,7 +17,7 @@ namespace Chocolade
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FrmMenu());
+            Application.Run(new Form1());
         }
     }
 }
