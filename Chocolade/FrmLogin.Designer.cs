b34566dc95f358bb77725f78169ff618bb52e964
Kristof Lettens <kristof.lettens@hotmail.com>
2022-01-06 23:07:43 +0100

    Login mooier gemaakt

--- Chocolade/FrmLogin.Designer.cs
+++ Chocolade/FrmLogin.Designer.cs
@@ -38,60 +38,79 @@ namespace Chocolade
             this.btnCancel = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.pctChocolate = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pctChocolate)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(29, 146);
+            this.label1.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(71, 232);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(119, 20);
+            this.label1.Size = new System.Drawing.Size(134, 19);
             this.label1.TabIndex = 0;
             this.label1.Text = "Gebruikersnaam:";
             // 
             // txtGebruikersnaam
             // 
-            this.txtGebruikersnaam.Location = new System.Drawing.Point(166, 143);
+            this.txtGebruikersnaam.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtGebruikersnaam.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtGebruikersnaam.Location = new System.Drawing.Point(71, 267);
             this.txtGebruikersnaam.Name = "txtGebruikersnaam";
-            this.txtGebruikersnaam.Size = new System.Drawing.Size(177, 27);
+            this.txtGebruikersnaam.Size = new System.Drawing.Size(299, 27);
             this.txtGebruikersnaam.TabIndex = 1;
+            this.txtGebruikersnaam.Text = "alala";
             // 
             // txtWachtwoord
             // 
-            this.txtWachtwoord.Location = new System.Drawing.Point(166, 192);
+            this.txtWachtwoord.BackColor = System.Drawing.SystemColors.Window;
+            this.txtWachtwoord.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtWachtwoord.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtWachtwoord.Location = new System.Drawing.Point(71, 353);
             this.txtWachtwoord.Name = "txtWachtwoord";
             this.txtWachtwoord.PasswordChar = '*';
-            this.txtWachtwoord.Size = new System.Drawing.Size(177, 27);
+            this.txtWachtwoord.Size = new System.Drawing.Size(299, 27);
             this.txtWachtwoord.TabIndex = 3;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(29, 195);
+            this.label2.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(71, 321);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(96, 20);
+            this.label2.Size = new System.Drawing.Size(105, 19);
             this.label2.TabIndex = 2;
             this.label2.Text = "Wachtwoord:";
             // 
             // btnLogin
             // 
-            this.btnLogin.Location = new System.Drawing.Point(29, 240);
+            this.btnLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(130)))));
+            this.btnLogin.FlatAppearance.BorderSize = 0;
+            this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogin.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnLogin.ForeColor = System.Drawing.Color.White;
+            this.btnLogin.Location = new System.Drawing.Point(71, 411);
             this.btnLogin.Name = "btnLogin";
-            this.btnLogin.Size = new System.Drawing.Size(135, 29);
+            this.btnLogin.Size = new System.Drawing.Size(314, 47);
             this.btnLogin.TabIndex = 4;
             this.btnLogin.Text = "Login";
-            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.UseVisualStyleBackColor = false;
             this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
             // 
             // btnCancel
             // 
-            this.btnCancel.Location = new System.Drawing.Point(208, 240);
+            this.btnCancel.BackColor = System.Drawing.Color.Transparent;
+            this.btnCancel.FlatAppearance.BorderSize = 0;
+            this.btnCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancel.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnCancel.ForeColor = System.Drawing.Color.Black;
+            this.btnCancel.Location = new System.Drawing.Point(371, 24);
             this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(135, 29);
+            this.btnCancel.Size = new System.Drawing.Size(54, 48);
             this.btnCancel.TabIndex = 5;
-            this.btnCancel.Text = "Cancel";
-            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Text = "X";
+            this.btnCancel.UseVisualStyleBackColor = false;
             this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
             // 
             // timer1
@@ -102,18 +121,30 @@ namespace Chocolade
             // pctChocolate
             // 
             this.pctChocolate.Image = global::Chocolade.Properties.Resources.Chocolate;
-            this.pctChocolate.Location = new System.Drawing.Point(125, 35);
+            this.pctChocolate.Location = new System.Drawing.Point(174, 109);
             this.pctChocolate.Name = "pctChocolate";
             this.pctChocolate.Size = new System.Drawing.Size(124, 92);
             this.pctChocolate.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pctChocolate.TabIndex = 6;
             this.pctChocolate.TabStop = false;
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Black", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(71, 40);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 32);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "LOGIN";
+            // 
             // FrmLogin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(371, 313);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(238)))), ((int)(((byte)(240)))), ((int)(((byte)(243)))));
+            this.ClientSize = new System.Drawing.Size(447, 533);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.pctChocolate);
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.btnLogin);
@@ -142,5 +173,6 @@ namespace Chocolade
         private System.Windows.Forms.Button btnCancel;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.PictureBox pctChocolate;
+        private System.Windows.Forms.Label label3;
     }
 }
